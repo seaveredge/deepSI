65c4f86a75aa71b0e3a70b3f965c1785f893d9da
seaveredge <50490905+seaveredge@users.noreply.github.com>
2022-02-23 17:52:16 +0100

    performance update

--- deepSI/utils/torch_nets.py
+++ deepSI/utils/torch_nets.py
@@ -44,15 +44,19 @@ class contracting_REN(nn.Module):
         # in:         | out:
         # - x (Nd, Nx)
         # - u (Nd, Nu)
+        Linv = torch.diag(1 / Lambda)
+        Dtilde = Linv @ calD_11
         # the following is of shape: (Nd, N_neurons)
         C1x_p_D12u_p_b = torch.einsum('ik, bk->bi', calC_1, x) + torch.einsum('ik, bk->bi', calD_12, u) + self.biasvec[self.n_state:(self.n_state+self.n_neurons)]
-        viis = [(1/Lambda[0])*C1x_p_D12u_p_b[:,0]]
-        w = torch.empty(x.shape[0], 0)
-        for i in range(1, self.n_neurons):
-            w = torch.cat((w,self.activation(torch.reshape(viis[-1],(-1,1)))), dim=1)
-            viis.append((1/Lambda[i])*(C1x_p_D12u_p_b[:,i]+
-                                       torch.einsum('j,bj->b',calD_11[i,:i],w)))
-        return self.activation(torch.stack(viis,dim=1)) #(Nd, N_neurons)
+        v = torch.einsum('ij,bj->bi',Linv, C1x_p_D12u_p_b)
+        #viis = [(1/Lambda[0])*C1x_p_D12u_p_b[:,0]]
+        #w = torch.empty(x.shape[0], 0)
+        for i in range(self.n_neurons):
+            v += torch.einsum('j,b->bj', Dtilde[:,i], self.activation(v[:,i]))
+            # w = torch.cat((w,self.activation(torch.reshape(viis[-1],(-1,1)))), dim=1)
+            # viis.append((1/Lambda[i])*(C1x_p_D12u_p_b[:,i]+
+            #                            torch.einsum('j,bj->b',calD_11[i,:i],w)))
+        return self.activation(v) #(Nd, N_neurons)
 
     def forward(self, hidden_state, u):
         # in:         | out:
